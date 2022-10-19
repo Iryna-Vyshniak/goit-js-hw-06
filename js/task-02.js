98116bf8e0cc1bb698957d07ab40cdc5acea6606
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-19 23:10:46 +0300

    add task-02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,33 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+// const ingredientsRef = document.querySelector("#ingredients");
+
+// const itemsRef = ingredients.map((ingredient) => {
+//   const item = document.createElement("li");
+//   item.textContent = ingredient;
+//   item.classList.add("item");
+//   return item;
+// });
+
+// ingredientsRef.append(...itemsRef);
+
+const ingredientsRefs = document.querySelector("#ingredients");
+const ingredientsMarkup = [];
+
+function createIngredientsList(ingredients = []) {
+  ingredients.map((elem) => {
+    const item = document.createElement("li");
+    item.textContent = elem;
+    item.classList.add("item");
+    ingredientsMarkup.push(item);
+  });
+  ingredientsRefs.append(...ingredientsMarkup);
+}
+
+createIngredientsList(ingredients);
