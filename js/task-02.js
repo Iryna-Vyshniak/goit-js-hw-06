82acd05f1e2d5322ce1384276afba6d0d94344cb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-19 23:12:47 +0300

    update task-02

--- js/task-02.js
+++ js/task-02.js
@@ -6,28 +6,28 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-// const ingredientsRef = document.querySelector("#ingredients");
+const ingredientsRef = document.querySelector("#ingredients");
 
-// const itemsRef = ingredients.map((ingredient) => {
-//   const item = document.createElement("li");
-//   item.textContent = ingredient;
-//   item.classList.add("item");
-//   return item;
-// });
+const itemsRef = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.classList.add("item");
+  return item;
+});
 
-// ingredientsRef.append(...itemsRef);
+ingredientsRef.append(...itemsRef);
 
-const ingredientsRefs = document.querySelector("#ingredients");
-const ingredientsMarkup = [];
+// const ingredientsRefs = document.querySelector("#ingredients");
+// const ingredientsMarkup = [];
 
-function createIngredientsList(ingredients = []) {
-  ingredients.map((elem) => {
-    const item = document.createElement("li");
-    item.textContent = elem;
-    item.classList.add("item");
-    ingredientsMarkup.push(item);
-  });
-  ingredientsRefs.append(...ingredientsMarkup);
-}
+// function createIngredientsList(ingredients = []) {
+//   ingredients.map((elem) => {
+//     const item = document.createElement("li");
+//     item.textContent = elem;
+//     item.classList.add("item");
+//     ingredientsMarkup.push(item);
+//   });
+//   ingredientsRefs.append(...ingredientsMarkup);
+// }
 
-createIngredientsList(ingredients);
+// createIngredientsList(ingredients);
