2412efd7781bb17ec5ae4ee6ce539bae088e3e90
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-23 23:07:06 +0300

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,42 @@
+/*
+Напиши скрипт управління формою логіна.
+
+<form class="login-form">
+  <label>
+    Email
+    <input type="email" name="email" />
+  </label>
+  <label>
+    Password
+    <input type="password" name="password" />
+  </label>
+  <button type="submit">Login</button>
+</form>
+
+Обробка відправлення форми form.login-form повинна відбуватися відповідно до події submit.
+Під час відправлення форми сторінка не повинна перезавантажуватися.
+Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
+Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
+Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+*/
+
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', submitHandler);
+const profile = {};
+
+function submitHandler(event) {
+    const email = event.currentTarget.elements.email;
+    const password = event.currentTarget.elements.password;
+
+    if (email.value === '' || password.value === '') {
+        alert("Please fill in all the fields!");
+    } else {
+        profile.email = email.value;
+        profile.password = password.value;
+        console.log(`Email: ${email.value},\nPassword: ${password.value}`);
+        console.log(profile);
+    }
+    event.currentTarget.reset();
+}
 
