600d2c2ac564b2a9887b8d518f065ce4edc4bc76
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2022-10-25 01:14:39 +0300

    add task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,77 @@
+/*
+Напиши скрипт створення і очищення колекції елементів. Користувач вводить кількість елементів в input і натискає кнопку Створити, після чого рендериться колекція. Натисненням на кнопку Очистити, колекція елементів очищається.
+
+<div id="controls">
+  <input type="number" min="1" max="100" step="1" />
+  <button type="button" data-create>Create</button>
+  <button type="button" data-destroy>Destroy</button>
+</div>
+
+<div id="boxes"></div>
+
+Створи функцію createBoxes(amount), яка приймає один параметр - число. Функція створює стільки <div>, скільки вказано в amount і додає їх у div#boxes.
+
+Розміри найпершого <div> - 30px на 30px.
+Кожен елемент після першого повинен бути ширшим і вищим від попереднього на 10px.
+Всі елементи повинні мати випадковий колір фону у форматі HEX. Використовуй готову функцію getRandomHexColor для отримання кольору.
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
+*/
+
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputNumberEl = document.querySelector('[type="number"]');
+const createBtnEl = document.querySelector('button[data-create]');
+const destroyBtnEl = document.querySelector('button[data-destroy]');
+const divBoxesEl = document.getElementById('boxes');
+divBoxesEl.style.display = 'flex';
+divBoxesEl.style.flexWrap = 'wrap';
+divBoxesEl.style.alignItems = 'center';
+divBoxesEl.style.marginTop = '30px';
+
+inputNumberEl.addEventListener('input', toogleValue);
+createBtnEl.addEventListener('click', () => {
+  createBoxes(Number(inputNumberEl.value));
+});
+destroyBtnEl.addEventListener('click', destroyBoxesMarkup);
+
+let inputValue = 0;
+function toogleValue(event) {
+  inputValue = event.currentTarget.value;
+  console.log(inputValue);
+}
+
+function createBoxes(amount) {
+  let divArray = [];
+  let sizeDefault = 30;
+  for (let i = 0; i < amount; i += 1) {
+    sizeDefault += 10;
+    const divElement = document.createElement("div");
+    divElement.classList = "item";
+    divElement.style.height = `${sizeDefault}px`;
+    divElement.style.width = `${sizeDefault}px`;
+    divElement.style.marginRight = '30px';
+    divElement.style.marginBottom = '30px';
+    divElement.style.backgroundColor = getRandomHexColor();
+    divArray.push(divElement);
+    // divBoxesEl.appendChild(divElement);
+  }
+  return divBoxesEl.append(...divArray);
+};
+
+function destroyBoxesMarkup() {
+  inputNumberEl.value = '';
+  return divBoxesEl.innerHTML = '';
+}
+
+
+
