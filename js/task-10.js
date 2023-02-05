5d5883635e1b59836927cc22bb452ba23f288fbd
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-06 00:07:31 +0200

    refactor script for creating and cleaning a collection of elements

--- js/task-10.js
+++ js/task-10.js
@@ -39,37 +39,71 @@ divBoxesEl.style.flexWrap = 'wrap';
 divBoxesEl.style.alignItems = 'center';
 divBoxesEl.style.marginTop = '30px';
 
-inputNumberEl.addEventListener('input', toogleValue);
-createBtnEl.addEventListener('click', () => {
-  createBoxes(Number(inputNumberEl.value));
-});
-destroyBtnEl.addEventListener('click', destroyBoxesMarkup);
+// inputNumberEl.addEventListener('input', toogleValue);
+// createBtnEl.addEventListener('click', () => {
+//   createBoxes(Number(inputNumberEl.value));
+// });
+// destroyBtnEl.addEventListener('click', destroyBoxesMarkup);
 
-let inputValue = 0;
-function toogleValue(event) {
-  inputValue = event.currentTarget.value;
-  console.log(inputValue);
-}
+// let inputValue = 0;
+// function toogleValue(event) {
+//   inputValue = event.currentTarget.value;
+//   console.log(inputValue);
+// }
+
+// function createBoxes(amount) {
+//   let divArray = [];
+//   let sizeDefault = 30;
+//   for (let i = 0; i < amount; i += 1) {
+//     sizeDefault += 10;
+//     const divElement = document.createElement('div');
+//     divElement.classList = 'item';
+//     divElement.style.height = `${sizeDefault}px`;
+//     divElement.style.width = `${sizeDefault}px`;
+//     divElement.style.marginRight = '30px';
+//     divElement.style.marginBottom = '30px';
+//     divElement.style.backgroundColor = getRandomHexColor();
+//     divArray.push(divElement);
+//     // divBoxesEl.appendChild(divElement);
+//   }
+//   return divBoxesEl.append(...divArray);
+// }
+
+// function destroyBoxesMarkup() {
+//   inputNumberEl.value = '';
+//   return (divBoxesEl.innerHTML = '');
+// }
+
+createBtnEl.addEventListener('click', () => {
+  console.log(inputNumberEl.value);
+  Number(inputNumberEl.value) > Number(inputNumberEl.max) ||
+  Number(inputNumberEl.value) < Number(inputNumberEl.min)
+    ? alert('Please enter number from 1 to 100')
+    : createBoxes(inputNumberEl.value);
+  inputNumberEl.value = '';
+});
+
+destroyBtnEl.addEventListener('click', destroyBoxes);
 
 function createBoxes(amount) {
-  let divArray = [];
-  let sizeDefault = 30;
+  let defaultSize = 30;
+  const boxesArr = [];
   for (let i = 0; i < amount; i += 1) {
-    sizeDefault += 10;
-    const divElement = document.createElement('div');
-    divElement.classList = 'item';
-    divElement.style.height = `${sizeDefault}px`;
-    divElement.style.width = `${sizeDefault}px`;
-    divElement.style.marginRight = '30px';
-    divElement.style.marginBottom = '30px';
-    divElement.style.backgroundColor = getRandomHexColor();
-    divArray.push(divElement);
-    // divBoxesEl.appendChild(divElement);
+    defaultSize += 10 * i;
+    const div = document.createElement('div');
+    div.classList = 'item';
+    div.style.width = `${defaultSize}px`;
+    div.style.height = `${defaultSize}px`;
+    div.style.marginRight = '30px';
+    div.style.marginBottom = '30px';
+    div.style.backgroundColor = getRandomHexColor();
+    boxesArr.push(div);
+    console.log('arr length', boxesArr.length);
   }
-  return divBoxesEl.append(...divArray);
+  return divBoxesEl.append(...boxesArr);
 }
 
-function destroyBoxesMarkup() {
+function destroyBoxes() {
   inputNumberEl.value = '';
-  return (divBoxesEl.innerHTML = '');
+  divBoxesEl.innerHTML = '';
 }
