464fb53678430d0c8302a13ba3f901c122684080
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-10 15:34:54 +0200

    add different variants to create markup

--- js/task-03.js
+++ js/task-03.js
@@ -13,12 +13,80 @@ const images = [
   },
 ];
 
-const galleryItems = ({ url, alt }) =>
-  `<li><img src="${url}" alt="${alt}" width = 300 height = auto></li>`;
+const gallery = document.querySelector('.gallery');
+
+// ------------- 1st variant ----------------------------------------------------------------
+// const galleryItems = ({ url, alt }) =>
+//   `<li><img src="${url}" alt="${alt}" width = 300 height = auto></li>`;
+// const galleryMarkup = images.reduce(
+//   (acc, item) => acc + galleryItems(item),
+//   []
+// );
+
+// gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+// ------------- 2nd variant ----------------------------------------------------------------
+/*
+const renderGallery = (arr) => {
+  return arr.map(({ url, alt }) => {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = url;
+    img.alt = alt;
+    img.width = 150;
+    li.append(img);
+    return li;
+  });
+};
+
+const galleryMarkup = renderGallery(images);
+console.log(galleryMarkup);
+
+gallery.append(...galleryMarkup);
+*/
+
+//------------- 3rd variant ----------------------------------------------------------------------
+/*
+const renderGallery = ({ url, alt }) =>
+  `<li><img src="${url}" alt="${alt}" width = "200" height = "auto"></li>`;
+
+const galleryMarkup = images.map(renderGallery).join('');
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+*/
+
+// ------------- 4th variant --------------------------------------------------------------------
+/*
+const galleryMarkup = images
+  .map(
+    ({ url, alt }) =>
+      `<li><img src="${url}" alt="${alt}" width = "250" height = "auto"></li>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+*/
+
+// ------------- 5th variant --------------------------------------------------------------------
+
 const galleryMarkup = images.reduce(
-  (acc, item) => acc + galleryItems(item),
-  []
+  (acc, { alt, url }) =>
+    (acc += `<li><img src="${url}" alt="${alt}" width = "300" height = "auto"></li>`),
+  ''
 );
 
-const gallery = document.querySelector('.gallery');
-gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// ------------- 6th variant --------------------------------------------------------------------
+/*
+const galleryMarkup = images
+  .reduce(
+    (acc, image) =>
+      acc.concat(
+        `<li><img src="${image.url}" alt="${image.alt}" width = "350" height = "auto"></li>`
+      ),
+    []
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+*/
