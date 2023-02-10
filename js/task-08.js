1f164a6078ea9ca6f1ed27d5a1d41f64258c1240
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-02-10 18:09:35 +0200

    update

--- js/task-08.js
+++ js/task-08.js
@@ -58,11 +58,11 @@ function handleFormSubmit(e) {
   if (email.value.trim() === '' || password.value.trim() === '') {
     return alert('Please fill in all the fields!');
   } else {
-    const formData = new FormData(e.currentTarget);
-
-    formData.forEach((value, name) => {
-      console.log(`${name} ${value}`);
-    });
+    const formData = {
+      email: email.value,
+      password: password.value,
+    };
+    console.log(formData);
+    e.currentTarget.reset();
   }
-  e.currentTarget.reset();
 }
